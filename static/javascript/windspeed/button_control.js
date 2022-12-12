e84a53c79aa3a6732437ad6b1e77aabbe56d1169
BDFD <bdfd2005@gmail.com>
2022-12-12 18:21:05 -0500

    Update button_control.js

--- static/javascript/windspeed/button_control.js
+++ static/javascript/windspeed/button_control.js
@@ -2,7 +2,7 @@
  * @Date         : 2022-12-12 12:48:56
  * @Author       : BDFD,bdfd2005@gmail.com
  * @Github       : https://github.com/bdfd
- * @LastEditTime : 2022-12-12 17:14:52
+ * @LastEditTime : 2022-12-12 18:19:13
  * @LastEditors  : BDFD
  * @Description  :
  * @FilePath     : \static\javascript\windspeed\button_control.js
@@ -21,8 +21,7 @@ console.log(waveForm);
 
 waveForm.o2[0].addEventListener("click", function () {
 	if (this.checked) {
-		waveForm.o2.value = 1;
-
+		waveForm.o2.value = 2;
 		for (let i = 0; i < requiredC1.length; i++) {
 			requiredC1[i].value = "";
 			requiredC2[i].value = "";
@@ -39,7 +38,6 @@ waveForm.o2[0].addEventListener("click", function () {
 waveForm.o2[1].addEventListener("click", function () {
 	if (this.checked) {
 		waveForm.o2.value = 2;
-
 		for (let i = 0; i < requiredC1.length; i++) {
 			requiredC1[i].value = "";
 			requiredC2[i].value = "";
@@ -52,14 +50,15 @@ waveForm.o2[1].addEventListener("click", function () {
 		}
 	}
 });
+
 waveForm.o2[2].addEventListener("click", function () {
 	if (this.checked) {
 		waveForm.o2.value = 3;
 		waveForm.o3[0].addEventListener("click", function () {
 			if (this.checked) {
-				waveForm.o3.value = 1;
-
+				// waveForm.o3.value = 1;
 				for (let i = 0; i < requiredC1.length; i++) {
+					waveForm.o3.value = 1;
 					requiredC1[i].value = "";
 					requiredC2[i].value = "";
 					requiredC1[i].value = "";
@@ -74,7 +73,6 @@ waveForm.o2[2].addEventListener("click", function () {
 		waveForm.o3[1].addEventListener("click", function () {
 			if (this.checked) {
 				waveForm.o3.value = 2;
-
 				for (let i = 0; i < requiredC1.length; i++) {
 					requiredC1[i].value = "";
 					requiredC2[i].value = "";
