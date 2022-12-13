967d23c4ad65d5e4c602c8b031795d93812045f2
BDFD <bdfd2005@gmail.com>
2022-12-13 16:39:11 -0500

    update windspeed input page layout

--- static/javascript/windspeed/button_control.js
+++ static/javascript/windspeed/button_control.js
@@ -2,7 +2,7 @@
  * @Date         : 2022-12-12 12:48:56
  * @Author       : BDFD,bdfd2005@gmail.com
  * @Github       : https://github.com/bdfd
- * @LastEditTime : 2022-12-13 12:16:54
+ * @LastEditTime : 2022-12-13 15:19:42
  * @LastEditors  : BDFD
  * @Description  :
  * @FilePath     : \static\javascript\windspeed\button_control.js
@@ -16,17 +16,10 @@ var requiredC2 = document.getElementsByClassName("requiredC2");
 var requiredC3 = document.getElementsByClassName("requiredC3");
 var requiredC4 = document.getElementsByClassName("requiredC4");
 
-waveForm.o3[1].value = " ";
-console.log(waveForm.o3[1].value);
-
 waveForm.o2[0].addEventListener("click", function () {
 	if (this.checked) {
 		waveForm.o2.value = 1;
-		// waveForm.o3[1].value = "0";
-		waveForm.o3[0].disabled = true;
-		waveForm.o3[1].disabled = true;
 		for (let i = 0; i < requiredC1.length; i++) {
-			waveForm.o3[i].value = "";
 			requiredC1[i].value = "";
 			requiredC2[i].value = "";
 			requiredC1[i].value = "";
@@ -42,8 +35,6 @@ waveForm.o2[0].addEventListener("click", function () {
 waveForm.o2[1].addEventListener("click", function () {
 	if (this.checked) {
 		waveForm.o2.value = 2;
-		waveForm.o3[0].disabled = true;
-		waveForm.o3[1].disabled = true;
 		for (let i = 0; i < requiredC1.length; i++) {
 			requiredC1[i].value = "";
 			requiredC2[i].value = "";
@@ -60,19 +51,6 @@ waveForm.o2[1].addEventListener("click", function () {
 waveForm.o2[2].addEventListener("click", function () {
 	if (this.checked) {
 		waveForm.o2.value = 3;
-		waveForm.o3[0].disabled = false;
-		waveForm.o3[1].disabled = false;
-		for (let i = 0; i < requiredC1.length; i++) {
-			requiredC1[i].value = "";
-			requiredC2[i].value = "";
-			requiredC1[i].disabled = true;
-			requiredC2[i].disabled = true;
-		}
-	}
-});
-waveForm.o3[0].addEventListener("click", function () {
-	if (this.checked) {
-		waveForm.o3.value = 1;
 		for (let i = 0; i < requiredC1.length; i++) {
 			requiredC1[i].value = "";
 			requiredC2[i].value = "";
@@ -85,9 +63,10 @@ waveForm.o3[0].addEventListener("click", function () {
 		}
 	}
 });
-waveForm.o3[1].addEventListener("click", function () {
+
+waveForm.o2[3].addEventListener("click", function () {
 	if (this.checked) {
-		waveForm.o3.value = 2;
+		waveForm.o2.value = 4;
 		for (let i = 0; i < requiredC1.length; i++) {
 			requiredC1[i].value = "";
 			requiredC2[i].value = "";
@@ -100,3 +79,47 @@ waveForm.o3[1].addEventListener("click", function () {
 		}
 	}
 });
+
+// waveForm.o2[2].addEventListener("click", function () {
+// 	if (this.checked) {
+// 		waveForm.o2.value = 3;
+// 		waveForm.o3[0].disabled = false;
+// 		waveForm.o3[1].disabled = false;
+// 		for (let i = 0; i < requiredC1.length; i++) {
+// 			requiredC1[i].value = "";
+// 			requiredC2[i].value = "";
+// 			requiredC1[i].disabled = true;
+// 			requiredC2[i].disabled = true;
+// 		}
+// 	}
+// });
+// waveForm.o3[0].addEventListener("click", function () {
+// 	if (this.checked) {
+// 		waveForm.o3.value = 1;
+// 		for (let i = 0; i < requiredC1.length; i++) {
+// 			requiredC1[i].value = "";
+// 			requiredC2[i].value = "";
+// 			requiredC1[i].value = "";
+// 			requiredC2[i].value = "";
+// 			requiredC1[i].disabled = true;
+// 			requiredC2[i].disabled = true;
+// 			requiredC3[i].disabled = false;
+// 			requiredC4[i].disabled = true;
+// 		}
+// 	}
+// });
+// waveForm.o3[1].addEventListener("click", function () {
+// 	if (this.checked) {
+// 		waveForm.o3.value = 2;
+// 		for (let i = 0; i < requiredC1.length; i++) {
+// 			requiredC1[i].value = "";
+// 			requiredC2[i].value = "";
+// 			requiredC1[i].value = "";
+// 			requiredC2[i].value = "";
+// 			requiredC1[i].disabled = true;
+// 			requiredC2[i].disabled = true;
+// 			requiredC3[i].disabled = true;
+// 			requiredC4[i].disabled = false;
+// 		}
+// 	}
+// });
