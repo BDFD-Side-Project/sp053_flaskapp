79bb2cef235e81c2aed48a3bdd70780b396b1829
BDFD <bdfd2005@gmail.com>
2022-12-13 11:33:48 -0500

    add test page

--- static/javascript/windspeed/button_control.js
+++ static/javascript/windspeed/button_control.js
@@ -2,7 +2,7 @@
  * @Date         : 2022-12-12 12:48:56
  * @Author       : BDFD,bdfd2005@gmail.com
  * @Github       : https://github.com/bdfd
- * @LastEditTime : 2022-12-12 18:19:13
+ * @LastEditTime : 2022-12-13 11:33:40
  * @LastEditors  : BDFD
  * @Description  :
  * @FilePath     : \static\javascript\windspeed\button_control.js
@@ -21,7 +21,7 @@ console.log(waveForm);
 
 waveForm.o2[0].addEventListener("click", function () {
 	if (this.checked) {
-		waveForm.o2.value = 2;
+		waveForm.o2.value = 1;
 		for (let i = 0; i < requiredC1.length; i++) {
 			requiredC1[i].value = "";
 			requiredC2[i].value = "";
@@ -56,9 +56,8 @@ waveForm.o2[2].addEventListener("click", function () {
 		waveForm.o2.value = 3;
 		waveForm.o3[0].addEventListener("click", function () {
 			if (this.checked) {
-				// waveForm.o3.value = 1;
+				waveForm.o3.value = 1;
 				for (let i = 0; i < requiredC1.length; i++) {
-					waveForm.o3.value = 1;
 					requiredC1[i].value = "";
 					requiredC2[i].value = "";
 					requiredC1[i].value = "";
